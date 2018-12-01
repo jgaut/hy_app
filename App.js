fa12ccfe878d312bc73fa4d379d985f23662bc05
Jérémy GAUTIER <gautierjeremy@gmail.com>
2018-12-01 22:28:08 +0100

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Go v2</Text>
+        <Text>Go v3</Text>
       </View>
     );
   }
