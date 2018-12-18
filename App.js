d88ac6f74b3e7c9150db4d4820add57bbc3db96e
Jérémy GAUTIER <gautierjeremy@gmail.com>
2018-12-18 21:42:33 +0100

    Update App.js

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-<View style={styles.container}>
+<View style={styles.form}>
   <Text style={styles.label} h4>Email</Text>
   <TextInput 
     label="Password" 
@@ -33,7 +33,8 @@ export default class App extends React.Component {
     value={this.state.password}
     onChangeText={password => this.setState({ password })}
     mode="outlined"/>
-
+</View>
+<View style={styles.button}>
    <TouchableOpacity onPress={this._onPressButton}>
           <View style={styles.button}>
             <Text style={styles.buttonText}>TouchableOpacity</Text>
@@ -49,11 +50,18 @@ function _onPressButton(){
 }
 
 const styles = StyleSheet.create({
-  container: {
+  form: {
     padding: 30, 
     backgroundColor: '#f5fcff',
     flex: 1,
     alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  button: {
+    padding: 30, 
+    backgroundColor: '#f5fcff',
+    flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
   },
   input: {
