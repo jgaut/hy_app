3020b07505da256dfcf35e0636d46c85b18c6d35
Jérémy GAUTIER <gautierjeremy@gmail.com>
2018-12-18 22:50:46 +0100

    Update App.js

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends React.Component {
     secureTextEntry={false} 
     style={styles.input} 
     value={this.state.email}
-    onChangeText={email => this.setState({ email }); email => this.setLabel({ "Email" }); }
+    onChangeText={email => this.setState({ email }), email => this.setLabel({ "Email" })}
     mode="outlined"/>
 
   <Text style={styles.label} h2>Password</Text>
