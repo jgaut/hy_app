6b78343bac985fa6b73b9894220dc21dedc51f4a
Jérémy GAUTIER <gautierjeremy@gmail.com>
2018-12-18 20:43:56 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import awsconfig from './aws-exports';
 import Auth from '@aws-amplify/auth';
@@ -7,46 +7,13 @@ import Auth from '@aws-amplify/auth';
 // retrieve temporary AWS credentials and sign requests
 Auth.configure(awsconfig);
 
-  var t = require('tcomb-form-native');
-
-  var Form = t.form.Form;
-
-  // here we are: define your domain model
-  var Person = t.struct({
-        email: t.String              // a required string
-  });
-
-  var Password = t.struct({
-        password: t.String              // a required string
-  });
-
-var options = {
-  fields: {
-    password: {
-      secureTextEntry: true
-    }
-  }
-};
-
-var options = {}; // optional rendering options (see documentation)
-
 export default class App extends React.Component {
 
 
   render() {
     return (
 <View style={styles.container}>
-        {/* display */}
-        <Form
-          ref="form"
-          type={Person}
-          options={options}
-        />
-        <Form
-          ref="form"
-          type={Password}
-          options={options}
-        />
+  <TextInput secureTextEntry={true} value="abc" />
 
       </View>
     );
