e17ef034c225792ed92e865259207cfa63d8722b
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-02-05 14:07:33 +0100

    Update TestPage.js

--- views/TestPage.js
+++ views/TestPage.js
@@ -23,7 +23,7 @@ class TestPageScreen extends React.Component {
 
   Story() {
   // Correct! JSX type can be a capitalized variable.
-  const SpecificStory = 'TextInput';
+  const SpecificStory = 'View';
   return <SpecificStory>ok</SpecificStory>;
 }
 
