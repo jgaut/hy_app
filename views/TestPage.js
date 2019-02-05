b96aefb95390dc7a9f57a8c59d92f06f35d687a8
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-02-05 14:38:10 +0100

    Update TestPage.js

--- views/TestPage.js
+++ views/TestPage.js
@@ -32,10 +32,10 @@ class TestPageScreen extends React.Component {
     
       if(item.type=='Text'){
         console.log(item.type);
-          returnValue.push(<Text>okokokok !!!!!</Text>);
+          returnValue.push(<Text style={styles.label}>okokokok !!!!!</Text>);
       }else if(item.type=='TextInput'){
         console.log(item.type);
-          returnValue.push(<TextInput>okokokok !!!!!</TextInput>);
+          returnValue.push(<TextInput style={styles.input}>okokokok !!!!!</TextInput>);
       }
     });
     return returnValue;
@@ -55,5 +55,39 @@ class TestPageScreen extends React.Component {
   
 }
 }
+
+const styles = StyleSheet.create({
+  form: {
+    padding: 80, 
+    backgroundColor: '#f5fcff',
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  label: {
+    color: '#000000'
+  },
+  button: {
+    marginTop:10,
+    paddingTop:40,
+    backgroundColor: '#2196F3',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+  },
+container: {
+    backgroundColor: '#f5fcff',
+    flex: 1,
+  },
+  input: {
+    height: 50,
+    backgroundColor: '#fff',
+    borderBottomWidth: 1, 
+    borderBottomColor: '#555' 
+  },
+});
 
 export default TestPageScreen;
