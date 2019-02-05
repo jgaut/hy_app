c6ae61429f4dd85080f21fb02f84b9c7f599feae
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-02-05 14:21:53 +0100

    Update TestPage.js

--- views/TestPage.js
+++ views/TestPage.js
@@ -35,7 +35,7 @@ class TestPageScreen extends React.Component {
   render() {
     
     var mydata = this.Story(this.state.data);
-    console.log(mydata);
+    //console.log(mydata);
 
     return (
       <View>{mydata}</View>
