d3dd243e9fa54cf5664b5f2ec70936b4c509a5ba
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-02-12 19:40:15 +0100

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,6 @@ import { createAppContainer } from 'react-navigation';
 import SignInUpScreen from './views/SignInUp';
 import SignUpConfirmScreen from './views/SignUpConfirm';
 import AppAuthScreen from './AppAuth';
-import TestPageScreen from './views/TestPage';
 
 // retrieve temporary AWS credentials and sign requests
 Auth.configure(awsconfig);
