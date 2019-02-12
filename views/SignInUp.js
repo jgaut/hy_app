828ab06551b34b6ce6e4b8004983f1b8bb7a506f
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-02-12 19:01:32 +0100

    Update SignInUp.js

--- views/SignInUp.js
+++ views/SignInUp.js
@@ -98,11 +98,6 @@ class SignInScreen extends React.Component {
               <Text style={styles.buttonText}>Sign in / Sign Up</Text>
             </View>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => { navigate('TestPage'); } }>
-                      <View style={styles.button}>
-              <Text style={styles.buttonText}>Try Test Page</Text>
-            </View>
-            </TouchableOpacity>
           </View>
 
         </KeyboardAvoidingView>
