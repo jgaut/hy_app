276e09b4dd401823e792575d14cbe13f56a9310a
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-02-13 21:19:38 +0100

    Update CreatePage.js

--- views/CreatePage.js
+++ views/CreatePage.js
@@ -32,6 +32,7 @@ class CreatePageScreen extends React.Component {
 
     if(this.state.isSav){
       this.state.fromKey = this.props.navigation.state.params.myKey;
+      console.log(this.state.fromKey);
       if(this.state.fromKey=='' || this.state.fromKey=='undefined'){
         const uuidv4 = require('uuid/v4');
         let myTmp = {"id":uuidv4(),"list":[]};
