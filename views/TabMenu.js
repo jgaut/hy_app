cc01e2c3379159a39863e3596043ab0c7d307b28
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-02-13 19:29:13 +0100

    Update TabMenu.js

--- views/TabMenu.js
+++ views/TabMenu.js
@@ -27,7 +27,7 @@ class TabMenuScreen extends React.Component {
 const MyTabNavigator = createBottomTabNavigator({
   'Journal': ListPageScreen,
   'Create Page': CreatePageScreen,
-  'Open': {console.log('open');}
+  'Open': {{console.log('open')}}
 }, 
 {
   navigationOptions: {
