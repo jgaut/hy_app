f990e37e7afdb1e281b69937a2c0260c4ea4d85f
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-02-18 16:01:22 +0100

    Update TabMenu.js

--- views/TabMenu.js
+++ views/TabMenu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {createAppContainer, createBottomTabNavigator, createStackNavigator} from 'react-navigation';
 import HomeScreen from './Home';
 import ProfileScreen from './Profile';
@@ -30,7 +30,7 @@ const MyTabNavigator = createBottomTabNavigator({
     screen: ListPageScreen,
     navigationOptions: () => ({
       tabBarLabel: () => {},
-      tabBarIcon: ({ tintColor }) => (<Icon name='grid_on' size={30} color="black"/>)
+      tabBarIcon: ({ tintColor }) => (<Icon name='calendar' size={30} color="black"/>)
     }),
   },
   'Create Page':{
