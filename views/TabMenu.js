111d4c2b5fc04c1961db52e85d04742b5639ace6
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-02-20 21:26:51 +0100

    Update TabMenu.js

--- views/TabMenu.js
+++ views/TabMenu.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Platform } from "react-native";
-import { View } from 'react-native';
+import { Platform, StyleSheet, View } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import {createAppContainer, createBottomTabNavigator, createStackNavigator} from 'react-navigation';
 import HomeScreen from './Home';
