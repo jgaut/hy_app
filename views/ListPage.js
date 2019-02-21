761482d07096d0e5a56a922d49a035eafed75676
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-02-21 20:31:14 +0100

    Update ListPage.js

--- views/ListPage.js
+++ views/ListPage.js
@@ -45,13 +45,10 @@ class ListPageScreen extends React.Component {
       .then(result => {
         result.forEach(item => {
           this.state.data.list.push(item);
-          //console.log('item : '+item.key);
         });
         this.forceUpdate();
       })
       .catch(err => console.log(err));
-      //console.log('big list : ' +this.state.data.list);
-      
   }
 
   render() {
