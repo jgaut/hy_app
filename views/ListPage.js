2e7aa734f38adb4e74042f2649bd3c6473d4b777
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-02-21 21:43:26 +0100

    Update ListPage.js

--- views/ListPage.js
+++ views/ListPage.js
@@ -14,7 +14,7 @@ class ListPageScreen extends React.Component {
     this.props.navigation.addListener('didFocus', () => {
      this.ListAllElement();
     });
-    this.state.width = (Dimensions.get('window').width/1)-2*2;
+    this.state.width = (Dimensions.get('window').width/8)-9*1;
   }
 
   Story(props) {
