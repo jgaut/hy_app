d926dd9899d6e3b85a9311a5b05a0876171a3625
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-01 23:50:26 +0100

    Update CreatePage.js

--- views/CreatePage.js
+++ views/CreatePage.js
@@ -164,7 +164,7 @@ class CreatePageScreen extends React.Component {
 
   render() {
     return (
-      <KeyboardAvoidingView  behavior="padding" style={styles.container}>
+      <KeyboardAvoidingView  behavior="padding" style={styles.container} keyboardVerticalOffset={90}>
         <View style={styles.form}>
           {this.Story(this.state.data)}
         </View>
