069ea5fc8cf31bdcf4c9397c9a99fa3610bf4288
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-01 20:32:13 +0100

    Update Camerarollpicker.js

--- views/Camerarollpicker.js
+++ views/Camerarollpicker.js
@@ -1,53 +1,82 @@
-import React from 'react';
-import { StyleSheet, View} from 'react-native';
-import CameraRollPicker from 'react-native-camera-roll-picker'
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-class HomeScreen extends React.Component {
+import CameraRollPicker from 'react-native-camera-roll-picker';
 
-  constructor() {
-    super();
- this.state = {
+class Roll extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
       num: 0,
       selected: [],
     };
+
+    this.getSelectedImages = this.getSelectedImages.bind(this);
   }
 
-  componentWillMount(){
-  }
+  getSelectedImages(images, current) {
+    var num = images.length;
 
-  getSelectedImages(a, b){
-    console.log(a);
-    console.log(b);
+    this.setState({
+      num: num,
+      selected: images,
+    });
+
+    console.log(current);
+    console.log(this.state.selected);
   }
 
   render() {
-    const {navigate} = this.props.navigation;
     return (
-      <View>
-<CameraRollPicker
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <Text style={styles.text}>
+            <Text style={styles.bold}> {this.state.num} </Text> images has been selected
+          </Text>
+        </View>
+        <CameraRollPicker
           groupTypes='SavedPhotos'
           maximum={3}
           selected={this.state.selected}
           assetType='Photos'
           imagesPerRow={3}
           imageMargin={5}
-          callback={this.getSelectedImages} />        </View>
-      
+          callback={this.getSelectedImages} />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  form: {
-    padding: 60, 
-    backgroundColor: '#f5fcff',
+  container: {
     flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'center',
+    backgroundColor: '#F6AE2D',
   },
-  label: {
-    paddingTop: 30
+  content: {
+    marginTop: 15,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  text: {
+    fontSize: 16,
+    alignItems: 'center',
+    color: '#fff',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  info: {
+    fontSize: 12,
   },
 });
 
-export default HomeScreen;
+export default Roll;
