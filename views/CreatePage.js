02420a5a6bd9a9f09b648b0839b52f01b65a5a46
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-03 15:24:56 +0100

    Update CreatePage.js

--- views/CreatePage.js
+++ views/CreatePage.js
@@ -120,7 +120,7 @@ class CreatePageScreen extends React.Component {
               >*/
               <View
                 key={Math.random()}
-                onMoveShouldSetResponderCapture={(evt, gestureState) => Math.abs(gestureState.dy) > 500}
+                onMoveShouldSetResponderCapture={(evt, gestureState) => {console.log(gestureState); return Math.abs(gestureState.dy) > 500}}
                 //onResponderMove={(event) => {console.log(JSON.stringify('move : '+event));}}
               >
               <TextInput 
