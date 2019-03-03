608aaf479dfdb4f08052aad72805ae9a75de3067
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-03 15:17:16 +0100

    Update CreatePage.js

--- views/CreatePage.js
+++ views/CreatePage.js
@@ -123,7 +123,7 @@ class CreatePageScreen extends React.Component {
                 onMoveShouldSetResponderCapture={() => true} 
                 //onResponderMove={(event) => {console.log(JSON.stringify('move : '+event));}}
                 onResponderRelease={(event) => {console.log(JSON.stringify('release : '+JSON.stringify(event.nativeEvent)));}}
-                onMoveShouldSetResponder={(event) => {console.log(JSON.stringify('responder : '+event));}}
+                onMoveShouldSetResponder={(event) => {console.log(JSON.stringify('responder : '+JSON.stringify(event.nativeEvent)));}}
               >
               <TextInput 
                 style={styles.note} 
