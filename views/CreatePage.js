54539e1b448f776bc32e3aa5e1f6218503ab1293
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-05 18:45:49 +0100

    Update CreatePage.js

--- views/CreatePage.js
+++ views/CreatePage.js
@@ -71,7 +71,7 @@ class Example extends Component {
           renderItem={this.renderItem}
           keyExtractor={(item, index) => `draggable-item-${item.key}`}
           scrollPercent={5}
-          onMoveEnd={({ data }) => {this.state.data.list=data; console.log(data);}}
+          onMoveEnd={({ data }) => {this.state.data.list=data; console.log(data); console.log(this.state.data.list);}}
         />
       </View>
     )
