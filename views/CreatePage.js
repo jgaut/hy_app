ed2f93e228b3dbcb1b30736bf7001849d7b5cad8
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-06 15:20:16 +0100

    Update CreatePage.js

--- views/CreatePage.js
+++ views/CreatePage.js
@@ -78,8 +78,8 @@ class CreatePage extends Component {
 
   renderItem = ({ item, index, move, moveEnd, isActive }) => {
     //console.log("item : " +item);
-    console.log("item : " +item.text||item.uri);
-    console.log("compo : " +JSON.stringify(this.state.isMoving));
+    //console.log("item : " +item.text||item.uri);
+    //console.log("compo : " +JSON.stringify(this.state.isMoving));
     switch(item.type) {
       case 'note':
         return (
