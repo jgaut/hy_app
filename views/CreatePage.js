0755761ffb10885e3b9d0078b8d948e9440e39c4
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-06 12:34:17 +0100

    Update CreatePage.js

--- views/CreatePage.js
+++ views/CreatePage.js
@@ -76,7 +76,7 @@ class CreatePage extends Component {
       .catch(err => console.log(err));
   }
 
-  renderItem = ({ item, index, move, moveEnd, isActive, component }) => {
+  renderItem = ({ item, index, move, moveEnd, isActive }) => {
     //console.log("item : " +item);
     console.log("item : " +item.text);
     switch(item.type) {
@@ -146,7 +146,7 @@ class CreatePage extends Component {
 
         <DraggableFlatList
           data={this.state.list}
-          renderItem={(item, index, move, moveEnd, isActive ) => {this.renderItem(item, index, move, moveEnd, isActive, this );}}
+          renderItem={(item, index, move, moveEnd, isActive ) => {this.renderItem(item, index, move, moveEnd, isActive);}}
           keyExtractor={(item, index) => `draggable-item-${item.key}`}
           scrollPercent={5}
           onMoveEnd={({ data }) => this.setState({ 'list': data})}
