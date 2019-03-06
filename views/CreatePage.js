8299357579388d6ceda14c93220e036e55e60af1
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-06 19:37:01 +0100

    Update CreatePage.js

--- views/CreatePage.js
+++ views/CreatePage.js
@@ -184,18 +184,12 @@ class CreatePage extends Component {
             </View>
           </TouchableOpacity>
         
-
         <TouchableOpacity key={Math.random()} onPress={() => {console.log("onPressButton");this.setState({'isMoving':this.state.isMoving?false:true});}}>
             <View style={styles.button}>
               <Text style={styles.buttonText}>{this.state.isMoving?"End move":"Start move"}</Text>
             </View>
           </TouchableOpacity>
 
-            <TouchableOpacity key={Math.random()} onPress={() => {console.log("onPressButton");this.setState({'isMoving':false});}}>
-            <View style={styles.button}>
-              <Text style={styles.buttonText}>End move</Text>
-            </View>
-          </TouchableOpacity>
         </View>
       </View>
     )
