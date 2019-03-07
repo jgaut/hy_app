00362efbc84d689acaa29f100a52fe71ce0d46ea
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-07 13:54:18 +0100

    Update EditNote.js

--- views/EditNote.js
+++ views/EditNote.js
@@ -26,12 +26,13 @@ class EditNote extends Component {
 
     return (
       <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <View>
         <TextInput 
           style={styles.note} 
           value={this.state.text}
           onChangeText={text => this.setState({ text })}
           />
-
+          <View style={styles.submitButton}>
           
           <TouchableOpacity key={Math.random()} onPress={() => {navigate('Create Page', {'type':'note', 'text':this.state.text});}}>
             <View style={styles.button}>
@@ -44,7 +45,8 @@ class EditNote extends Component {
               <Text style={styles.buttonText}>Cancel</Text>
             </View>
           </TouchableOpacity>
-    
+        </View>
+        </View>
       </KeyboardAvoidingView>
     );
   }
